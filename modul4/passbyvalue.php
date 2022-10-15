9e001baa634f1c0ff63134ae44dfcab2140156d0
Ika Nurfitriani <90813508+ikanurfitriani@users.noreply.github.com>
2022-10-15 17:45:32 +0700

    Delete modul4 directory

--- modul4/passbyvalue.php
+++
@@ -1,8 +0,0 @@
-<?php
-function jumlah($nilai) {
-	$nilai++;
-}
-$input=20;
-jumlah($input);
-echo $input;
-?>
--- modul4/strlen.php
+++
@@ -1,5 +0,0 @@
-<?php
-	$kata = "STMIK Akakom Yogyakarta";
-	$jumlah = strlen($kata);
-	echo $jumlah;
-?>
