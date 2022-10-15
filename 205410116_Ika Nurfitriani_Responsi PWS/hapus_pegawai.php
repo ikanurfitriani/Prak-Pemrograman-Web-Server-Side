0e009273b749af7be76ba1a05c433365b7d01410
Ika Nurfitriani <90813508+ikanurfitriani@users.noreply.github.com>
2022-10-15 17:44:13 +0700

    Delete 205410116_Ika Nurfitriani_Responsi PWS directory

--- 205410116_Ika Nurfitriani_Responsi PWS/hapus_pegawai.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'koneksi.php';
-
-$Id = $_GET['Id'];
-
-$result = mysqli_query($koneksi, "DELETE FROM pegawai WHERE Id='$Id'");
-	
-header('Location: tampil_pegawai.php');
-?>
--- 205410116_Ika Nurfitriani_Responsi PWS/koneksi.php
+++
@@ -1,7 +0,0 @@
-<?php
-$koneksi = mysqli_connect("localhost","root","","ika_nurfitriani");
-
-if (mysqli_connect_errno()) {
-	echo "Koneksi database gagal : " .mysqli_connect_error();
-}
-?>
