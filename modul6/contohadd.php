d42219a8b482959d5a7a4db1d5d8708a44f81a55
Ika Nurfitriani <90813508+ikanurfitriani@users.noreply.github.com>
2022-10-15 17:45:49 +0700

    Delete modul6 directory

--- modul6/contohadd.php
+++
@@ -1,6 +0,0 @@
-<!DOCTYPE html>
-<?php
-$str = addslashes('Mahasiswa/i Stmik Akakom belajar "coding" web server');
-
-echo($str);
-?>
--- modul6/testent.php
+++
@@ -1,7 +0,0 @@
-<?php
-$str = "<p>AKAKOM -&gt; &quot;</p>\n";
-
-echo htmlspecialchars_decode($str);
-
-echo htmlspecialchars_decode($str, ENT_NOQUOTES);
-?>
